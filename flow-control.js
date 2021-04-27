c338d8cb6376bf5f844e59b8b0966b0cffdc258b
Rheina <rheina.tamara@outlook.com>
2021-04-27 03:32:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,10 @@
-function basicTeenager(){
-  if(basicTeenager === 13 <= 19){
-    return "You are a teenager!"
-  }
-}
-function basicTeenager(){
-  if(basicTeenager === 13 < 19){
-    return "You are a teenager!"
-  }
-}
+// function basicTeenager(){
+//   if(basicTeenager === 13 <= 19){
+//     return "You are a teenager!"
+//   }
+// }
+// function basicTeenager(){
+//   if(basicTeenager === 13 < 19){
+//     return "You are a teenager!"
+//   }
+// }
