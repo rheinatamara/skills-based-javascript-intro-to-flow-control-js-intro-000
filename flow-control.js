9595f500b6a748e6967decdf566c58e5e9f24b34
Rheina <rheina.tamara@outlook.com>
2021-04-27 04:38:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,3 +18,10 @@ function ageChecker(age){
   }
   return 'You are a grownup';
 }
+
+function ternaryTeenager(age){
+  if(age >= 13 && age <= 19){
+    return 'You are a teenager';
+  }
+  return 'You are not a teenager'
+}
